11d514223b96456a9647e4caefa568001b2bd92e
Carina Vogl <vogl.carina@arcor.de>
2017-06-16 12:46:25 +0200

    New size of Island1

--- src/track.cpp
+++ src/track.cpp
@@ -4,5 +4,5 @@ Track::Track()
 {
     // Create the scene
     setSceneRect(0,0,1280,720); // make the scene 2000x2000 instead of infinity by infinity (default)
-    setBackgroundBrush(QBrush(QImage(":/images/images/background.jpeg")));
+    setBackgroundBrush(QBrush(QImage(":/images/Island1.png")));
 }
