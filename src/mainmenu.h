9197b77a063b52d380297961e716f78315ecff02
Michael Maas <maas.m@gmx.net>
2017-06-23 23:43:41 +0200

    Menu adjusts to screensize; Sliders in Garage are changed; Monza.png is deleted from res.qrc

--- src/mainmenu.h
+++ src/mainmenu.h
@@ -22,59 +22,65 @@ public:
 
 private slots:
 
-    void on_Main2Level1_clicked();
+    void on_main2Credits_clicked();
 
-    void on_Main2Garage_clicked();
+    void on_main2Garage_clicked();
 
-    void on_Main2Highscore_clicked();
+    void on_main2Highscore_clicked();
 
-    void on_Main2Settings_clicked();
+    void on_main2Level1_clicked();
 
-    void on_Main2Manual_clicked();
+    void on_main2Manual_clicked();
 
-    void on_Main2Credits_clicked();
+    void on_main2QuitGame_clicked();
 
-    void on_Main2QuitGame_clicked();
+    void on_main2Settings_clicked();
 
-    void on_Level1_2Main_clicked();
+    void on_level1_2Level2_clicked();
 
-    void on_Level2_2Main_clicked();
+    void on_level1_2Level3_clicked();
 
-    void on_Level3_2Main_clicked();
+    void on_level1_2Main_clicked();
 
-    void on_Garage2Main_clicked();
+    void on_level1_2Play_clicked();
 
-    void on_Highscore2Main_clicked();
+    void on_level2_2Level1_clicked();
 
-    void on_Settings2Main_clicked();
+    void on_level2_2Level3_clicked();
 
-    void on_Manual2Main_clicked();
+    void on_level2_2Main_clicked();
 
-    void on_Credits2Main_clicked();
+    void on_level2_2Play_clicked();
 
-    void on_Level1_2Level2_clicked();
+    void on_level3_2Level1_clicked();
 
-    void on_Level1_2Level3_clicked();
+    void on_level3_2Level2_clicked();
 
-    void on_Level2_2Level1_clicked();
+    void on_level3_2Main_clicked();
 
-    void on_Level2_2Level3_clicked();
+    void on_level3_2Play_clicked();
 
-    void on_Level3_2Level1_clicked();
+    void on_garage2Main_clicked();
 
-    void on_Level3_2Level2_clicked();
+    void on_highscore2Main_clicked();
 
-    void on_playLevel3_clicked();
+    void on_settings2Main_clicked();
 
-    void on_playLevel2_clicked();
+    void on_manual2Main_clicked();
 
-    void on_playLevel1_clicked();
+    void on_credits2Main_clicked();
 
-    void on_sliderAcceleration_valueChanged(int value);
+    void on_garageAccelerationSlider_valueChanged(int value);
 
-    void on_sliderTopspeed_valueChanged(int value);
+    void on_garageAccelerationSlider_sliderReleased();
 
-    void on_sliderHandling_valueChanged(int value);
+    void on_garageTopspeedSlider_valueChanged(int value);
+
+    void on_garageTopspeedSlider_sliderReleased();
+
+    void on_garageHandlingSlider_valueChanged(int value);
+
+    void on_garageHandlingSlider_sliderReleased();
 
 private:
     Ui::mainMenu *ui;
