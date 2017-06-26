6ab8c0f874e9a5d5c280b81a6778c3941668dd25
Johann Brenner <ga78zan@mytum.de>
2017-06-26 15:12:52 +0200

    Added race timer to game

--- src/world.h
+++ src/world.h
@@ -4,6 +4,8 @@
 #include <QGraphicsView>
 #include <Box2D/Box2D.h>
 #include <QTimer>
+#include <QTime>
+#include <QElapsedTimer>
 #include <QObject>
 #include <QKeyEvent>
 #include "track.h"
@@ -14,34 +16,44 @@
 
 class World : public QGraphicsView
 {
-    Q_OBJECT
+	Q_OBJECT
 
 private:
-    int mWidth;                 // Width of world/scene
-    int mHeight;                // Height of world/scene
-    int level;
-    Track* mTrack;
-    Car* mCar;
-    enum InputState mCurrentInputState;
+	int mWidth;                 // Width of world/scene
+	int mHeight;                // Height of world/scene
+	int level;
+	Track* mTrack;
+	Car* mCar;
+	enum InputState mCurrentInputState;
 
-    b2World* mWorld;             // World object for physic engine
+	b2World* mWorld;             // World object for physic engine
 
-    QTimer*mStartTimer;         // This timer counts the start sequence
-    QTimer* mTimer;             // This timer repeatly calls the game loop method
-    int mLoopIntervalTime;      //
+	QTimer*mStartTimer;         // This timer counts the start sequence
+	QTimer* mTimer;             // This timer repeatly calls the game loop method
+	int mLoopIntervalTime;      //
 
-    int mStartCounter;              // remaining start sequence Time in 10msec steps
-    float Opacity;                  // Opacity for fade out effect of StartCounger
-    QGraphicsTextItem* mCounter;    // Display Start Counter
+	int mStartCounter;              // remaining start sequence Time in 10msec steps
+	float Opacity;                  // Opacity for fade out effect of StartCounger
+	QGraphicsTextItem* mCounter;    // Display Start Counter
+
+	QGraphicsTextItem* mLabel;		//! contains the String with the elapsed time
+	QGraphicsTextItem* mRaceLabel;	//! contains the label in the scene
+	QPoint mLabelPos;				//! position of the label realtively to the window
+	QElapsedTimer mRaceTime;		//! computes the elapsed time since "GO!" in ms
+	int mElapsed = 0;
+	QTime mTime;					//! for translating ms dynamically into mm:ss.zzz
+	QElapsedTimer mPauseTime;		//! Computes the time of pause pressed
+
+	void updateTime();
 
 public:
-    World(int width, int height, int level);
-    void keyPressEvent(QKeyEvent * keyEvent);
-    void keyReleaseEvent(QKeyEvent * keyEvent);
+	World(int width, int height, int level);
+	void keyPressEvent(QKeyEvent * keyEvent);
+	void keyReleaseEvent(QKeyEvent * keyEvent);
 
 public slots:
-    void gameLoop();
-    void startLoop();
+	void gameLoop();
+	void startLoop();
 };
 
 #endif // WORLD_H
