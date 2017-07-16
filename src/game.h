37fddd3b1dc21a54d6df3558de9773a6ad5cc862
Adrian Schiechel <adrian.schiechel@tum.de>
2017-07-16 13:21:19 +0200

    Added doku for game.h, world,h and worldposition.h

--- src/game.h
+++ src/game.h
@@ -12,6 +12,7 @@
 //    YasMarina
 //};
 
+//! Loads the game and connect player with world class
 class Game: public QObject
 {
 	Q_OBJECT
@@ -21,12 +22,24 @@ private:
     bool mMultiplayer;
 
 public:
+    //! Generates world with standard screen size
     Game();
+
+    //! Generates world with selected screen size
     Game(int screenWidth, int screenHeight, bool fullscreen);
     ~Game();
+
+    //! Opens selected .circuit file and reads all necessary values
+    /*! The read values are passed to world class
+     * \param circuit Circuit enum values which are defined in player.h
+     * \param speedValue Garage value for topspeed
+     * \param accelerationValue Garage value for acceleration
+     * \param handlingValue Garage value for handling
+     */
     void loadCircuit(enum Circuit circuit, int speedValue, int accelerationValue, int handlingValue);
 
 public slots:
+    //! Slot to select multi- or singleplayer
     void setMultiplayer(bool mp);
 };
 
